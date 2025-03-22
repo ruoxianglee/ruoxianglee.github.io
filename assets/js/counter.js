de95d64314a61cb59c614db6497953de3fa2b6b9
ruoxianglee <ruoxiang.li@my.cityu.edu.hk>
2025-03-23 02:50:38 +0800

    update counter

---
+++ assets/js/counter.js
@@ -0,0 +1,12 @@
+// Counter code from xtrastats.com
+document.write(`
+    <img border="0" hspace="0" vspace="0" 
+      src="https://www.xtrastats.com/counter.php?i=25990
+      &r=${escape(document.referrer)}
+      &n=${escape(navigator.userAgent)}
+      &p=${escape(navigator.userAgent)}
+      &g=${escape(document.location.href)}
+      &sd=${screen.colorDepth}
+      &sw=${screen.width}x${screen.height}"
+    >
+  `);
